2ca8a1d4e3572f5fffce7dd4dfe747c35af8f004
Ztaoufiq <z.taoufiq@gmail.com>
2021-06-03 10:18:59 +0200

    First commit of my frontend angular snake cube solver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NameService } from './name.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-snake-cube';
+  startFrom="";
+  vectors="";
+  error="";
+  constructor(private nameService:NameService) {}
+
+  onSend(coordinateX:string,coordinateY:string,coordinateZ:string,segment:string){
+	const formData : FormData = new FormData()
+	let dimension = [coordinateX,coordinateY,coordinateZ]
+	formData.append('dimension',JSON.stringify(dimension))
+	formData.append('structure',segment)
+	this.nameService.onSendService(formData).subscribe
+	(res=>{
+		console.log(res);
+		this.startFrom= res[0][0];
+		this.vectors= res[0][1];
+	},
+	err=>{
+		console.log(err);
+		this.error= err.error.text
+	}
+	)
+}
 }
